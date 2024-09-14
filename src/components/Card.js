34e99f0917c7d69fe633f685225063f37c9ffccd
George <killyen444@gmail.com>
2024-09-14 14:54:30 +0800

    chore:updated image route

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ function Card() {
     return (
         <div className='cards'>
 
-            <h1>Check out these EPIC Destinations! </h1>
+            <h1>Check out these EPIC Destinations!! </h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
